0973cfe60d8ed0a98270674dbe2f57f986c5e7fe
usy <contact.usyless@gmail.com>
2026-01-08 16:42:00 +0000

    more tests

--- src/test.cpp
+++ src/test.cpp
@@ -14,12 +14,17 @@ int main() {
     print::println("strings::concat_strings (chars): {}", strings::concat_strings("hello", " ", "there!"));
     print::println(L"strings::concat_strings (wide chars): {}", strings::concat_strings(L"hello", L" ", L"there!"));
     {
-        auto lol = "THis IS moSTLY upperCASE";
-        print::println("strings::to_lowercase before: {}, after: {}", lol, strings::to_lowercase(lol));
+        auto str = "THis IS moSTLY upperCASE";
+        print::println("strings::to_lowercase before: {}, after: {}", str, strings::to_lowercase(str));
     }
     {
-        auto lol = "this has THIS STrING and once again THIS STrING";
-        print::println("strings::replace_all before: {}, after: {}", lol, strings::replace_all(lol, "THIS STrING", "not that string"));
+        auto str = "this has THIS STrING and once again THIS STrING";
+        print::println("strings::replace_all before: {}, after: {}", str, strings::replace_all(str, "THIS STrING", "not that string"));
+    }
+    print::println("strings::to_number_positive<size_t> {}", *strings::to_number_positive<size_t>("1234567"));
+    {
+        auto str = "?this_is_a_get=lol a space??&ts=!!!%";
+        print::println("strings::url_encode before: {}, after: {}", str, strings::url_encode(str));
     }
 
     #ifdef WIN32
