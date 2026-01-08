ee748c08b14a500d1ae6394f0b997b74fbfa3dfa
usy <contact.usyless@gmail.com>
2026-01-08 01:28:06 +0000

    add recycle file

--- include/usylibpp/windows.hpp
+++ include/usylibpp/windows.hpp
@@ -3,6 +3,9 @@
 #include <string>
 #include <filesystem>
 #include <windows.h>
+#include <shobjidl.h>
+#include <shlguid.h>
+#include <wrl/client.h>
 
 namespace usylibpp::windows {
     inline std::wstring to_wstr(const char* utf8) {
@@ -40,5 +43,48 @@ namespace usylibpp::windows {
 
     inline std::string to_utf8(const std::filesystem::path& p) {
         return to_utf8(p.native().c_str());
+    }
+
+    class COMWrapper {
+    private:
+        HRESULT hr;
+    public:
+        COMWrapper() : hr(CoInitializeEx(NULL, COINIT_APARTMENTTHREADED | COINIT_DISABLE_OLE1DDE)) {}
+
+        HRESULT status() {
+            return hr;
+        }
+
+        ~COMWrapper() {
+            if (SUCCEEDED(hr)) CoUninitialize();
+        }
+    };
+
+    inline bool recycle_file(const wchar_t* wstr) {
+        using Microsoft::WRL::ComPtr;
+
+        COMWrapper COM{};
+        if (FAILED(COM.status())) return false;
+
+        ComPtr<IFileOperation> fileOp;
+        HRESULT hr = CoCreateInstance(CLSID_FileOperation, nullptr, CLSCTX_ALL, IID_PPV_ARGS(fileOp.GetAddressOf()));
+        if (FAILED(hr)) return false;
+
+        ComPtr<IShellItem> item;
+        hr = SHCreateItemFromParsingName(wstr, nullptr, IID_PPV_ARGS(item.GetAddressOf()));
+        if (FAILED(hr)) return false;
+
+        fileOp->SetOperationFlags(FOFX_RECYCLEONDELETE);
+
+        hr = fileOp->DeleteItem(item.Get(), nullptr);
+        if (FAILED(hr)) return false;
+        
+        hr = fileOp->PerformOperations();
+        if (FAILED(hr)) return false;
+
+        BOOL anyFailed = FALSE;
+        fileOp->GetAnyOperationsAborted(&anyFailed);
+
+        return !anyFailed;
     }
 }
