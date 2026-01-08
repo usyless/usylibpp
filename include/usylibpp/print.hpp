94a5375a2c6e0696e68dd4626d4276b8af6694fb
usy <contact.usyless@gmail.com>
2026-01-08 16:48:22 +0000

    change println into print and add new println

--- include/usylibpp/print.hpp
+++ include/usylibpp/print.hpp
@@ -9,16 +9,27 @@ namespace usylibpp::print {
      * Prints to cout or wcout depending on the args passed in
      */
     template<typename Fmt = const char*, typename... Ts>
-    inline void println(Fmt&& fmt = "", Ts&&... args) {
+    inline void print(Fmt&& fmt = "", Ts&&... args) {
         // using Char = typename std::basic_string_view<std::remove_cvref_t<decltype(fmt[0])>>::value_type;
         using Char = std::remove_cvref_t<decltype(fmt[0])>;
 
         if constexpr (std::is_same_v<Char, char>) {
-            std::cout << std::vformat(std::forward<Fmt>(fmt), std::make_format_args(args...)) <<'\n';
+            std::cout << std::vformat(std::forward<Fmt>(fmt), std::make_format_args(args...));
         } else if constexpr (std::is_same_v<Char, wchar_t>) {
-            std::wcout << std::vformat(std::forward<Fmt>(fmt), std::make_wformat_args(args...)) << L'\n';
+            std::wcout << std::vformat(std::forward<Fmt>(fmt), std::make_wformat_args(args...));
         } else {
-            static_assert(!std::is_same_v<Char, Char>, "Unsupported type passed to usylibpp::print::println");
+            static_assert(!std::is_same_v<Char, Char>, "Unsupported type passed to usylibpp::print::print");
         }
+    }
+
+    /**
+     * Will fail silently on mismatched paranthesis and args
+     * Prints to cout or wcout depending on the args passed in
+     */
+    template<typename Fmt = const char*, typename... Ts>
+    inline void println(Fmt&& fmt = "", Ts&&... args) {
+        using Char = std::remove_cvref_t<decltype(fmt[0])>;
+
+        print(fmt, args..., Char('\n'));
     }
 }
