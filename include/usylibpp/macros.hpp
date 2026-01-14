1fe2e72e008c1fe6f9ebbb21a993bc45c9e2e2f5
usy <contact.usyless@gmail.com>
2026-01-14 16:10:19 +0000

    mark or_default functions as constexpr

--- include/usylibpp/macros.hpp
+++ include/usylibpp/macros.hpp
@@ -3,18 +3,18 @@
 #define USYLIBPP__MAKE_OR(function, or) \
 template <typename... TArgs, typename... Call> \
 requires (sizeof...(TArgs) > 0) \
-[[nodiscard]] inline auto function##_or_default(Call&&... args) { \
+[[nodiscard]] inline constexpr auto function##_or_default(Call&&... args) { \
     return function<TArgs...>(std::forward<Call>(args)...).value_or(or); \
 } \
  \
 template <auto... NTArgs, typename... Call> \
 requires (sizeof...(NTArgs) > 0) \
-[[nodiscard]] inline auto function##_or_default(Call&&... args) { \
+[[nodiscard]] inline constexpr auto function##_or_default(Call&&... args) { \
     return function<NTArgs...>(std::forward<Call>(args)...).value_or(or); \
 } \
  \
 template <typename... Call> \
 requires (true) \
-[[nodiscard]] inline auto function##_or_default(Call&&... args) { \
+[[nodiscard]] inline constexpr auto function##_or_default(Call&&... args) { \
     return function(std::forward<Call>(args)...).value_or(or); \
 } \
