a2eec39229e36c88938a0455ecabbe058feb6bcc
usy <contact.usyless@gmail.com>
2026-02-14 03:31:40 +0000

    end timer on destruct

--- include/usylibpp/util/timer.hpp
+++ include/usylibpp/util/timer.hpp
@@ -7,13 +7,19 @@ namespace usylibpp::util {
 class Timer {
 private:
     const std::chrono::high_resolution_clock::time_point start = std::chrono::high_resolution_clock::now();
+    bool ended{false};
 public:
-    inline void end() const noexcept {
+    inline void end() noexcept {
+        ended = true;
         const auto end = std::chrono::high_resolution_clock::now();
         const auto duration_ms = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
         const auto duration_us = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
         const std::chrono::duration<double> duration = end - start;
         print::println("Time taken: {}, {}, ({:.9f} seconds)", duration_ms, duration_us, duration.count());
+    }
+
+    ~Timer() {
+        if (!ended) end();
     }
 };
 }
