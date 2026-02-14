76b432188ec39e9d63b6b81d8b367c7b8d3068eb
usy <contact.usyless@gmail.com>
2026-02-14 14:28:20 +0000

    add shorter aliases, tell clangd header is including exports

--- include/usylibpp/usylibpp.hpp
+++ include/usylibpp/usylibpp.hpp
@@ -1,35 +1,55 @@
 #pragma once
+// NOLINTBEGIN(misc-unused-alias-decls)
 
-#include "macros.hpp"
-#include "opts.hpp"
+#include "macros.hpp" // IWYU pragma: export
+#include "opts.hpp" // IWYU pragma: export
+
+namespace ulp = usylibpp;
 
 #ifdef USYLIBPP_ENABLE_WINDOWS
-#include "windows.hpp"
-#include "windows/fs.hpp"
-#include "windows/process.hpp"
-#include "windows/strings.hpp"
+#include "windows.hpp" // IWYU pragma: export
+#include "windows/fs.hpp" // IWYU pragma: export
+#include "windows/process.hpp" // IWYU pragma: export
+#include "windows/strings.hpp" // IWYU pragma: export
+
+namespace usylibpp {
+    namespace win = ulp::windows;
+}
+namespace usylibpp::windows {
+    namespace proc = win::process;
+}
 #endif
 
-#include "discord.hpp"
-#include "strings.hpp"
-#include "files.hpp"
-#include "init.hpp"
-#include "time.hpp"
-#include "print.hpp"
-#include "util/worker.hpp"
-#include "util/timer.hpp"
+#include "discord.hpp" // IWYU pragma: export
+#include "strings.hpp" // IWYU pragma: export
+namespace usylibpp {
+    namespace str = ulp::strings;
+}
+#include "files.hpp" // IWYU pragma: export
+namespace usylibpp {
+    namespace fs = ulp::files;
+}
+#include "init.hpp" // IWYU pragma: export
+#include "time.hpp" // IWYU pragma: export
+namespace usylibpp {
+    namespace tm = ulp::time;
+}
+#include "print.hpp" // IWYU pragma: export
+#include "util/worker.hpp" // IWYU pragma: export
+#include "util/timer.hpp" // IWYU pragma: export
 #include <usylibppconfig.hpp>
 
 #ifdef USYLIBPP_ENABLE_WINTOAST
 #include <wintoastlib.h>
-#include "wintoasthelper.hpp"
+#include "wintoasthelper.hpp" // IWYU pragma: export
 #endif
 
 #ifdef USYLIBPP_ENABLE_WINDOWS_IMAGING
-#include "windows/images.hpp"
+#include "windows/images.hpp" // IWYU pragma: export
 #endif
 
 #ifdef USYLIBPP_ENABLE_TASK_DIALOG_DARK_MODE
-#include "windows/darkmode.hpp"
-#include "windows/task_dialog.hpp"
+#include "windows/darkmode.hpp" // IWYU pragma: export
+#include "windows/task_dialog.hpp" // IWYU pragma: export
 #endif
+// NOLINTEND(misc-unused-alias-decls)
