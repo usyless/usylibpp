a9ef759eca65a58f0a8d5bb0f58f621f7dfe8a42
usy <contact.usyless@gmail.com>
2026-02-16 15:23:27 +0000

    improve print and println

--- include/usylibpp/print.hpp
+++ include/usylibpp/print.hpp
@@ -1,39 +1,29 @@
 #pragma once
 
 #include "aliases.hpp" // IWYU pragma: exports
-#include "types.hpp"
 #include <iostream>
 #include <format>
 
 namespace usylibpp::print {
-    /**
-     * Will fail silently on mismatched paranthesis and args
-     * Prints to cout or wcout depending on the args passed in
-     */
-    template<types::is_basic_string_view Fmt, typename... Ts>
-    inline auto& print(Fmt&& fmt = "", Ts&&... args) {
-        // using Char = typename std::basic_string_view<std::remove_cvref_t<decltype(fmt[0])>>::value_type;
-        using Char = types::string_view_char_t<Fmt>;
-
-        if constexpr (std::is_same_v<Char, char>) {
-            std::cout << std::vformat(std::forward<Fmt>(fmt), std::make_format_args(args...));
-            return std::cout;
-        } else if constexpr (std::is_same_v<Char, wchar_t>) {
-            std::wcout << std::vformat(std::forward<Fmt>(fmt), std::make_wformat_args(args...));
-            return std::wcout;
-        } else {
-            static_assert(!std::is_same_v<Char, Char>, "Unsupported type passed to usylibpp::print::print");
-        }
+    template<typename... Ts>
+    inline auto& print(const std::format_string<Ts...>& fmt, Ts&&... args) {
+        std::cout << std::vformat(fmt.get(), std::make_format_args(args...));
+        return std::cout;
     }
 
-    /**
-     * Will fail silently on mismatched paranthesis and args
-     * Prints to cout or wcout depending on the args passed in
-     */
-    template<types::is_basic_string_view Fmt = decltype(""), typename... Ts>
-    inline void println(Fmt&& fmt = "", Ts&&... args) {
-        using Char = types::string_view_char_t<Fmt>;
+    template<typename... Ts>
+    inline auto& print(const std::wformat_string<Ts...>& fmt, Ts&&... args) {
+        std::wcout << std::vformat(fmt.get(), std::make_wformat_args(args...));
+        return std::wcout;
+    }
 
-        print(std::forward<Fmt>(fmt), std::forward<Ts>(args)...) << Char('\n');
+    template<typename... Ts>
+    inline void println(const std::format_string<Ts...>& fmt = "", Ts&&... args) {
+        print(fmt, std::forward<Ts>(args)...) << '\n';
+    }
+
+    template<typename... Ts>
+    inline void println(const std::wformat_string<Ts...>& fmt, Ts&&... args) {
+        print(fmt, std::forward<Ts>(args)...) << L'\n';
     }
 }
