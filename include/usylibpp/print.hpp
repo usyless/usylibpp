497ca646891293e09e4a9af65c99e7d3ad11f19e
usy <contact.usyless@gmail.com>
2026-02-16 15:27:50 +0000

    if c++23 just use std::print and std::println instead in print

--- include/usylibpp/print.hpp
+++ include/usylibpp/print.hpp
@@ -1,10 +1,18 @@
 #pragma once
 
 #include "aliases.hpp" // IWYU pragma: exports
+#if __cplusplus >= 202302L
+#include <print>
+#else
 #include <iostream>
 #include <format>
+#endif
 
 namespace usylibpp::print {
+    #if __cplusplus >= 202302L
+    using std::println;
+    using std::print;
+    #else
     template<typename... Ts>
     inline auto& print(const std::format_string<Ts...>& fmt, Ts&&... args) {
         std::cout << std::vformat(fmt.get(), std::make_format_args(args...));
@@ -18,7 +26,7 @@ namespace usylibpp::print {
     }
 
     template<typename... Ts>
-    inline void println(const std::format_string<Ts...>& fmt = "", Ts&&... args) {
+    inline void println(const std::format_string<Ts...>& fmt, Ts&&... args) {
         print(fmt, std::forward<Ts>(args)...) << '\n';
     }
 
@@ -26,4 +34,9 @@ namespace usylibpp::print {
     inline void println(const std::wformat_string<Ts...>& fmt, Ts&&... args) {
         print(fmt, std::forward<Ts>(args)...) << L'\n';
     }
+
+    inline void println() {
+        std::cout << '\n';
+    }
+    #endif
 }
