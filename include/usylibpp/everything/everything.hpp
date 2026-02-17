0caaeb6950c1d49ca75d316b1497a3168ef0ced6
usy <contact.usyless@gmail.com>
2026-02-17 22:08:51 +0000

    dont use worker

--- include/usylibpp/everything/everything.hpp
+++ include/usylibpp/everything/everything.hpp
@@ -3,7 +3,6 @@
 #ifdef USYLIBPP_ENABLE_VOIDTOOLS_EVERYTHING
 
 #include "../aliases.hpp" // IWYU pragma: export
-#include "../util/worker.hpp"
 #include "../windows/process.hpp"
 #include "../strings.hpp"
 
@@ -81,10 +80,6 @@ namespace usylibpp::everything {
      */
     struct Everything {
     private:
-        util::Worker<util::WorkerOpts{
-            .drain_queue_on_cancel = true,
-            .type = util::WorkerType::ReturnDefault
-        }> worker{1};
         std::wstring instance_name;
         std::wstring everything_path = L"everything.exe";
         
@@ -125,12 +120,10 @@ namespace usylibpp::everything {
 
         template <bool wait_for_completion = true>
         [[nodiscard]] inline auto close_everything() {
-            return worker.post<wait_for_completion>([this]{
-                Everything_Exit();
-                Everything_CleanUp();
+            Everything_Exit();
+            Everything_CleanUp();
 
-                reset_wndclass();
-            });
+            reset_wndclass();
         }
 
         ~Everything() {
@@ -148,95 +141,98 @@ namespace usylibpp::everything {
             UACRejected
         };
         
-        template <bool wait_for_completion = true>
-        [[nodiscard]] inline auto try_load() {
-            return worker.post<wait_for_completion>([this]() -> LoadStatus {
-                if (everything_path.empty()) {
-                    return LoadStatus::NoExeFound;
-                }
-
-                const auto args = strings::concat_strings(L"-instance \"", instance_name, L"\" -admin -startup -is-run-as");
-                auto status = windows::process::run_admin_process<{
-                    .allow_visible_windows = true,
-                }>({
-                    .filename = &everything_path,
-                    .args = &args
-                });
-
-                if (status.status != windows::process::admin_process_output::Status::Success) {
-                    if (status.status == windows::process::admin_process_output::Status::UACRejected) {
-                        return LoadStatus::UACRejected;
-                    }
-                    return LoadStatus::OtherError;
-                }
-
-                std::this_thread::sleep_for(std::chrono::seconds(1));
-
-                wndclass = std::make_unique<std::wstring>(strings::concat_strings(EVERYTHING_IPC_WNDCLASS, L"_(", instance_name, L")"));
-
-                _Everything_IPC_WndClass = wndclass->c_str();
-
-                while (true) {
-                    if (Everything_IsDBLoaded()) {
-                        return LoadStatus::Success;
-                    } else if (Everything_GetLastError()) {
-                        // IPC not running.
-                        reset_wndclass();
-                        return LoadStatus::NotRunning;
-                    }
-                    
-                    // wait for database to load..
-                    std::this_thread::sleep_for(std::chrono::seconds(1));
-                }
-
-                reset_wndclass();
-                return LoadStatus::NotRunning;
-            });
-        }
-
-        template <bool wait_for_completion = true>
-        [[nodiscard]] inline auto is_loaded() {
-            return worker.post<wait_for_completion>([]() -> bool {
-                return Everything_IsDBLoaded();
-            });
-        }
-
-        template <bool wait_for_completion = true>
-        [[nodiscard]] inline auto reset_results() {
-            return worker.post<wait_for_completion>([]{
-                Everything_Reset();
-            });
-        }
-
-        template <bool wait_for_completion = true>
-        [[nodiscard]] inline auto do_query(const std::wstring& query, const SearchOptions& options = {}) {
-            constexpr auto do_query = [](const std::wstring& query, const SearchOptions& options) {
-                Everything_Reset();
-
-                Everything_SetMatchPath(options.MatchPath);
-                Everything_SetMatchCase(options.MatchCase);
-                Everything_SetMatchWholeWord(options.MatchWholeWord);
-                Everything_SetRegex(options.Regex);
-                Everything_SetMax(options.Max);
-                Everything_SetOffset(options.Offset);
-                // Everything_SetReplyWindow(options.ReplyWindow);
-                Everything_SetReplyID(options.ReplyID);
-                Everything_SetSort(options.Sort);
-                Everything_SetRequestFlags(options.RequestFlags);
-
-                Everything_SetSearch(query.c_str());
-
-                return Everything_Query(TRUE);
-            };
-            if constexpr (wait_for_completion) {
-                return worker.post<wait_for_completion>([&query, &options]{
-                    return do_query(query, options);
-                });
-            } else {
-                return worker.post<wait_for_completion>([query, options]{
-                    return do_query(query, options);
-                });
+        [[nodiscard]] inline LoadStatus try_load() {
+            if (everything_path.empty()) {
+                return LoadStatus::NoExeFound;
             }
+
+            const auto args = strings::concat_strings(L"-instance \"", instance_name, L"\" -admin -startup -is-run-as");
+            auto status = windows::process::run_admin_process<{
+                .allow_visible_windows = true,
+            }>({
+                .filename = &everything_path,
+                .args = &args
+            });
+
+            if (status.status != windows::process::admin_process_output::Status::Success) {
+                if (status.status == windows::process::admin_process_output::Status::UACRejected) {
+                    return LoadStatus::UACRejected;
+                }
+                return LoadStatus::OtherError;
+            }
+
+            std::this_thread::sleep_for(std::chrono::seconds(1));
+
+            wndclass = std::make_unique<std::wstring>(strings::concat_strings(EVERYTHING_IPC_WNDCLASS, L"_(", instance_name, L")"));
+
+            _Everything_IPC_WndClass = wndclass->c_str();
+
+            while (true) {
+                if (Everything_IsDBLoaded()) {
+                    return LoadStatus::Success;
+                } else if (Everything_GetLastError()) {
+                    // IPC not running.
+                    reset_wndclass();
+                    return LoadStatus::NotRunning;
+                }
+                
+                // wait for database to load..
+                std::this_thread::sleep_for(std::chrono::seconds(1));
+            }
+
+            reset_wndclass();
+            return LoadStatus::NotRunning;
+        }
+
+        [[nodiscard]] static inline auto is_loaded() {
+            return Everything_IsDBLoaded();
+        }
+
+        inline void static reset_results() {
+            Everything_Reset();
+        }
+
+        [[nodiscard]] static inline auto get_file_resuts_count() {
+            return Everything_GetNumFileResults();
+        }
+
+        [[nodiscard]] static inline auto get_folder_results_count() {
+            return Everything_GetNumFolderResults();
+        }
+
+        [[nodiscard]] static inline auto get_resuts_count() {
+            return Everything_GetNumResults();
+        }
+
+        [[nodiscard]] static inline auto get_total_file_results() {
+            return Everything_GetTotFileResults();
+        }
+
+        [[nodiscard]] static inline auto get_total_folder_results() {
+            return Everything_GetTotFolderResults();
+        }
+
+        [[nodiscard]] static inline auto get_total_results() {
+            return Everything_GetTotResults();
+        }
+
+        [[nodiscard]] static inline auto do_query(const std::wstring& query, const SearchOptions& options = {}) {
+            Everything_Reset();
+
+            Everything_SetMatchPath(options.MatchPath);
+            Everything_SetMatchCase(options.MatchCase);
+            Everything_SetMatchWholeWord(options.MatchWholeWord);
+            Everything_SetRegex(options.Regex);
+            Everything_SetMax(options.Max);
+            Everything_SetOffset(options.Offset);
+            // Everything_SetReplyWindow(options.ReplyWindow);
+            Everything_SetReplyID(options.ReplyID);
+            Everything_SetSort(options.Sort);
+            Everything_SetRequestFlags(options.RequestFlags);
+
+            Everything_SetSearch(query.c_str());
+
+            return Everything_Query(TRUE);
         }
     };
 }
