84d6066af6410815a7cdd6a941e2f3be2b88dc8c
usy <contact.usyless@gmail.com>
2026-03-06 04:05:56 +0000

    dont use keyword or in macro

--- include/usylibpp/macros.hpp
+++ include/usylibpp/macros.hpp
@@ -1,23 +1,23 @@
 #pragma once
 
-#define USYLIBPP__MAKE_OR(function, or) \
+#define USYLIBPP__MAKE_OR(function, default_value) \
 template <typename... TArgs, typename... Call> \
 requires (sizeof...(TArgs) > 0) \
 [[nodiscard]] inline constexpr auto function##_or_default(Call&&... args) \
-    noexcept(noexcept(function<TArgs...>(std::forward<Call>(args)...).value_or(or))) { \
-    return function<TArgs...>(std::forward<Call>(args)...).value_or(or); \
+    noexcept(noexcept(function<TArgs...>(std::forward<Call>(args)...).value_or(default_value))) { \
+    return function<TArgs...>(std::forward<Call>(args)...).value_or(default_value); \
 } \
  \
 template <auto... NTArgs, typename... Call> \
 requires (sizeof...(NTArgs) > 0) \
 [[nodiscard]] inline constexpr auto function##_or_default(Call&&... args) \
-    noexcept(noexcept(function<NTArgs...>(std::forward<Call>(args)...).value_or(or))) { \
-    return function<NTArgs...>(std::forward<Call>(args)...).value_or(or); \
+    noexcept(noexcept(function<NTArgs...>(std::forward<Call>(args)...).value_or(default_value))) { \
+    return function<NTArgs...>(std::forward<Call>(args)...).value_or(default_value); \
 } \
  \
 template <typename... Call> \
 requires (true) \
 [[nodiscard]] inline constexpr auto function##_or_default(Call&&... args) \
-    noexcept(noexcept(function(std::forward<Call>(args)...).value_or(or))) { \
-    return function(std::forward<Call>(args)...).value_or(or); \
+    noexcept(noexcept(function(std::forward<Call>(args)...).value_or(default_value))) { \
+    return function(std::forward<Call>(args)...).value_or(default_value); \
 } \
