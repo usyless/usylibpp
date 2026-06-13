5d52325379524af28ae9741c2cad7d8560c2c63c
usy <contact.usyless@gmail.com>
2026-06-13 12:08:07 +0100

    use pragma keep not export

--- include/usylibpp/pch_top_level.hpp
+++ include/usylibpp/pch_top_level.hpp
@@ -1,64 +1,64 @@
 #ifdef USYLIBPP_ENABLE_WINDOWS
-#include <windows.h> // IWYU pragma: export
-#include <shobjidl.h> // IWYU pragma: export
-#include <shlguid.h> // IWYU pragma: export
-#include <shellapi.h> // IWYU pragma: export
-#include <knownfolders.h> // IWYU pragma: export
-#include <shlobj.h> // IWYU pragma: export
+#include <windows.h> // IWYU pragma: keep
+#include <shobjidl.h> // IWYU pragma: keep
+#include <shlguid.h> // IWYU pragma: keep
+#include <shellapi.h> // IWYU pragma: keep
+#include <knownfolders.h> // IWYU pragma: keep
+#include <shlobj.h> // IWYU pragma: keep
 #endif
 
 #ifdef USYLIBPP_ENABLE_WINDOWS_IMAGING
-#include <wincodec.h> // IWYU pragma: export
-#include <wincodecsdk.h> // IWYU pragma: export
+#include <wincodec.h> // IWYU pragma: keep
+#include <wincodecsdk.h> // IWYU pragma: keep
 #endif
 
 #ifdef USYLIBPP_ENABLE_WIL
-#include <shellapi.h> // IWYU pragma: export
-#include <wil/resource.h> // IWYU pragma: export
-#include <wil/com.h> // IWYU pragma: export
+#include <shellapi.h> // IWYU pragma: keep
+#include <wil/resource.h> // IWYU pragma: keep
+#include <wil/com.h> // IWYU pragma: keep
 #endif
 
 #ifdef USYLIBPP_ENABLE_WINTOAST
-#include <wintoastlib.h> // IWYU pragma: export
+#include <wintoastlib.h> // IWYU pragma: keep
 #endif
 
 #ifdef USYLIBPP_ENABLE_LINUX
-#include <unistd.h> // IWYU pragma: export
-#include <sys/types.h> // IWYU pragma: export
-#include <sys/wait.h> // IWYU pragma: export
-#include <fcntl.h> // IWYU pragma: export
+#include <unistd.h> // IWYU pragma: keep
+#include <sys/types.h> // IWYU pragma: keep
+#include <sys/wait.h> // IWYU pragma: keep
+#include <fcntl.h> // IWYU pragma: keep
 #if defined(__linux__)
-#include <sys/prctl.h> // IWYU pragma: export
+#include <sys/prctl.h> // IWYU pragma: keep
 #endif
 #endif
 
-#include <cerrno> // IWYU pragma: export
-#include <chrono> // IWYU pragma: export
-#include <csignal> // IWYU pragma: export
-#include <concepts> // IWYU pragma: export
-#include <cstdint> // IWYU pragma: export
-#include <type_traits> // IWYU pragma: export
-#include <filesystem> // IWYU pragma: export
-#include <memory> // IWYU pragma: export
-#include <string> // IWYU pragma: export
-#include <optional> // IWYU pragma: export
-#include <thread> // IWYU pragma: export
-#include <atomic> // IWYU pragma: export
-#include <future> // IWYU pragma: export
-#include <mutex> // IWYU pragma: export
-#include <queue> // IWYU pragma: export
-#include <condition_variable> // IWYU pragma: export
-#include <functional> // IWYU pragma: export
-#include <algorithm> // IWYU pragma: export
-#include <cwctype> // IWYU pragma: export
-#include <string_view> // IWYU pragma: export
-#include <cstring> // IWYU pragma: export
-#include <charconv> // IWYU pragma: export
-#include <span> // IWYU pragma: export
-#include <fstream> // IWYU pragma: export
-#include <iostream> // IWYU pragma: export
-#include <locale> // IWYU pragma: export
-#include <format> // IWYU pragma: export
-#include <ctime> // IWYU pragma: export
-#include <iomanip> // IWYU pragma: export
-#include <sstream> // IWYU pragma: export
+#include <cerrno> // IWYU pragma: keep
+#include <chrono> // IWYU pragma: keep
+#include <csignal> // IWYU pragma: keep
+#include <concepts> // IWYU pragma: keep
+#include <cstdint> // IWYU pragma: keep
+#include <type_traits> // IWYU pragma: keep
+#include <filesystem> // IWYU pragma: keep
+#include <memory> // IWYU pragma: keep
+#include <string> // IWYU pragma: keep
+#include <optional> // IWYU pragma: keep
+#include <thread> // IWYU pragma: keep
+#include <atomic> // IWYU pragma: keep
+#include <future> // IWYU pragma: keep
+#include <mutex> // IWYU pragma: keep
+#include <queue> // IWYU pragma: keep
+#include <condition_variable> // IWYU pragma: keep
+#include <functional> // IWYU pragma: keep
+#include <algorithm> // IWYU pragma: keep
+#include <cwctype> // IWYU pragma: keep
+#include <string_view> // IWYU pragma: keep
+#include <cstring> // IWYU pragma: keep
+#include <charconv> // IWYU pragma: keep
+#include <span> // IWYU pragma: keep
+#include <fstream> // IWYU pragma: keep
+#include <iostream> // IWYU pragma: keep
+#include <locale> // IWYU pragma: keep
+#include <format> // IWYU pragma: keep
+#include <ctime> // IWYU pragma: keep
+#include <iomanip> // IWYU pragma: keep
+#include <sstream> // IWYU pragma: keep
